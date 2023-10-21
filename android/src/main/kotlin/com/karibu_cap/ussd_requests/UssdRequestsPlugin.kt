5c6bd0f79be8f94a9c9633279e6368630dc5148d
nidovic <feutseludovic@gmail.com>
2023-10-21 15:48:56 +0100

    Resolve dependency.

--- android/src/main/kotlin/com/karibu_cap/ussd_requests/UssdRequestsPlugin.kt
+++ android/src/main/kotlin/com/karibu_cap/ussd_requests/UssdRequestsPlugin.kt
@@ -19,6 +19,7 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import java.util.concurrent.CompletableFuture
+import io.flutter.plugin.common.EventChannel
 
 
 /** UssdRequestsPlugin */
